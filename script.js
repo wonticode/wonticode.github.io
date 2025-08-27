748e41077db421c2a191ee00eb39dc13c6a37e49
En <159270816+enneha@users.noreply.github.com>
2025-08-28 01:19:35 +0530

    changed layout

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 const darkMode = document.getElementById("dark-mode");
 const lightMode = document.getElementById("light-mode");
-const aboutBtn = document
-  .getElementById("aboutBtn")
-  .addEventListener("click", () => {
-    document.getElementById("about").className = "container active";
-    document.getElementById("project").className = "container";
-    document.getElementById("contact").className = "container";
-  });
-const projectBtn = document
-  .getElementById("projectBtn")
-  .addEventListener("click", () => {
-    document.getElementById("project").className = "container active";
-    document.getElementById("contact").className = "container";
-    document.getElementById("about").className = "container";
-  });
-
-const contactBtn = document
-  .getElementById("contactBtn")
-  .addEventListener("click", () => {
-    document.getElementById("project").className = "container";
-    document.getElementById("contact").className = "container active";
-    document.getElementById("about").className = "container";
-  });
 
 darkMode.addEventListener("click", () => {
   lightMode.style.setProperty("display", "inline-block");
@@ -34,8 +12,3 @@ lightMode.addEventListener("click", () => {
   darkMode.style.setProperty("display", "inline-block");
   document.getElementById("body").className = " ";
 });
-
-const activeContainer = document.querySelector(".container.active");
-if (activeContainer) {
-  activeContainer.classList.add("show-on-load");
-}
